2e90c2372fdafc2e138827ddd2a357bb1b45a422
Grant Canty <gc933815@ohio.edu>
2025-10-12 01:40:39 +0200

    changed util file. format matches what process_documents function expects

--- backend/util.py
+++ backend/util.py
@@ -1,17 +1,21 @@
 import logging
 from pathlib import Path
+import os
 
 
 logger = logging.getLogger(__name__)
 
 def get_available_text_files():
-    available_text_files = []
+    available_text_files = {}
     s = Path('./backend/documents')
-    for _, _, files in s.walk():
+    for root, _, files in s.walk():
         for file in files:
             path = Path(file.lower())
-            if path.endswith(".txt"):
-                available_text_files.append(path)
+            if path.suffix == ".txt":
+                file_path = Path(root, file)
+                f = open(file_path)
+                available_text_files[file] = f.read()
+                f.close()
     
 
     return available_text_files
