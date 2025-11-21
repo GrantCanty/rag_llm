305b118002c64e386ae02775afe1db2bf2bacb61
Grant Canty <gc933815@ohio.edu>
2025-11-21 14:42:25 +0100

    changed llm model and respective functions to generate text. changed the docker structure to no longer have to build the model after each change in text. added new modules to import

--- Dockerfile
+++ Dockerfile
@@ -13,12 +13,11 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the rest of your app code
-COPY . .
+# COPY . .
 
 # Expose the port FastAPI runs on
 EXPOSE 8501
 
-RUN ls
-
 # Command to start the app
 CMD ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
+#CMD ["python3", "main.py"]
--- backend/llm_query.py
+++ backend/llm_query.py
@@ -1,37 +1,56 @@
 from .chroma import query_similar_documents
-from .process_document import process_documents
-from .util import get_available_text_and_files, logger
-from huggingface_hub import InferenceClient
-import os
-from dotenv import load_dotenv
+from .util import logger
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-load_dotenv()
+LLM_MODEL = "/app/qwen_model"
 
-HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
-if HUGGINGFACE_API_TOKEN == None:
-    raise ValueError("Missing HuggingFace API Token ")
-LLM_MODEL = os.getenv("LLM_MODEL")
-if LLM_MODEL == None:
-    raise ValueError("Missing LLM Model Parameter")
+print("Starting tokenizer load...")
+tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL)
+print("Tokenizer loaded. Starting model load...")
 
-client = InferenceClient(model=LLM_MODEL, token=HUGGINGFACE_API_TOKEN, timeout=120)
+print("Starting model load...")
+model = AutoModelForCausalLM.from_pretrained(
+    LLM_MODEL,
+    dtype="auto",
+    device_map="auto"
+)
+print("Model loaded successfully!")
 
 def prompt_llm(prompt: str):
+    SYSTEM_PROMPT = "You are a Personal assistant specialized in providing a well formated answer from the context being provided by the user."
+    messages = [
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': prompt}
+    ]
+
     try:
-        SYSTEM_PROMPT = "You are a Personal assistant specialized in providing well formated answer from the context being provided by the user."
-        messages = [
-            {'role': 'system', 'content': SYSTEM_PROMPT},
-            {'role': 'user', 'content': prompt}
-        ]
-        result = client.chat.completions.create(
-            model=LLM_MODEL,
-            messages=messages,
-            temperature=0.1,
-            max_tokens=200
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True,
+            enable_thinking=False
         )
-        text = result.choices[0].message.content
-        return text
+        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+        # conduct text completion
+        generated_ids = model.generate(
+            **model_inputs,
+            max_new_tokens=16384
+        )
+        output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+
+        content = tokenizer.decode(output_ids, skip_special_tokens=True)
+
+        try:
+            # rindex finding 151668 (</think>)
+            index = len(output_ids) - output_ids[::-1].index(151668)
+        except ValueError:
+            index = 0
+
+        content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+        return content
     except Exception as e:
         logger.exception(f"Error when prompting LLM: {e}")
         return "Could not process your query"
